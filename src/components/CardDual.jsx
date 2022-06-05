ef9a246bf345edd6e2069e49411ec6b3736a0477
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-06-05 22:09:22 +0300

    Update CardDual.jsx

--- src/components/CardDual.jsx
+++ src/components/CardDual.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ReactLoading from "react-loading";
 
 export const CardDual = ({ data: { github, linkedin } }) => {
   const [gitData, setGitData] = useState([]);
